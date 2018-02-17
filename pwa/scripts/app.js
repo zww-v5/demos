1ec3b2c9817a99780a92baca4e1eb1a7c8f88b4b
zww_v5 <zhuweiwu@foxmail.com>
2018-02-17 09:35:10 +0800

    [*] 调整资源地址

--- pwa/scripts/app.js
+++ pwa/scripts/app.js
@@ -352,7 +352,7 @@
 
   // TODO add service worker code here
   if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('/service-worker.js').then(function() {
+      navigator.serviceWorker.register('/demos/pwa/service-worker.js').then(function() {
           console.log('Service Worker Registered');
       });
   }
