1a0cc19064ef6adeefdc1aefd3d756228aea0e3c
zww_v5 <zhuweiwu@foxmail.com>
2018-03-11 14:11:40 +0800

    [*] 优化样式

--- community-mobile-pwa/temp-pwa/service-worker.js
+++ community-mobile-pwa/temp-pwa/service-worker.js
@@ -2,25 +2,25 @@ var cacheName = 'Tencent-cloud-1.0';
 var filesToCache = [];
 var dataCacheName = 'Tencent-cloud-data-1.0';
 
-self.addEventListener('install', function(e) {
-    console.log('[ServiceWorker] Install');
-    e.waitUntil(caches.open(cacheName).then(function(cache) {
-        console.log('[ServiceWorker] Caching app shell');
-        return cache.addAll(filesToCache);
-    }));
-});
-
-self.addEventListener('activate', function(e) {
-    console.log('[ServiceWorker] Activate');
-    e.waitUntil(caches.keys().then(function(keyList) {
-        return Promise.all(keyList.map(function(key) {
-            console.log('[ServiceWorker] Removing old cache', key);
-            if (key !== cacheName && key !== dataCacheName) {
-                return caches.delete(key);
-            }
-        }));
-    }));
-});
+// self.addEventListener('install', function(e) {
+//     console.log('[ServiceWorker] Install');
+//     e.waitUntil(caches.open(cacheName).then(function(cache) {
+//         console.log('[ServiceWorker] Caching app shell');
+//         return cache.addAll(filesToCache);
+//     }));
+// });
+//
+// self.addEventListener('activate', function(e) {
+//     console.log('[ServiceWorker] Activate');
+//     e.waitUntil(caches.keys().then(function(keyList) {
+//         return Promise.all(keyList.map(function(key) {
+//             console.log('[ServiceWorker] Removing old cache', key);
+//             if (key !== cacheName && key !== dataCacheName) {
+//                 return caches.delete(key);
+//             }
+//         }));
+//     }));
+// });
 
 self.addEventListener('fetch', function(e) {
     e.respondWith(caches.match(e.request).then(function(response) {
