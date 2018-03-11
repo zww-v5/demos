88c08da7565c60c52e9c239b08a2138beea4b786
zww_v5 <zhuweiwu@foxmail.com>
2018-03-11 13:29:46 +0800

    [*] 优化样式

--- community-mobile-pwa/temp-pwa/service-worker.js
+++ community-mobile-pwa/temp-pwa/service-worker.js
@@ -1,5 +1,18 @@
 var cacheName = 'Tencent-cloud-1.0';
-var filesToCache = ['index.html','app.js','../css/community-m-dist.css','../css/sprite/community-m-201803072039.svg','../css/sprite/community-m-201803072039.png'];
+var filesToCache = [
+    'index.html','app.js',
+    'https://imgcache.qq.com/open_proj/proj_qcloud_v2/gateway/portal/css/global.css',
+    '../css/community-m-dist.css',
+    '../css/sprite/community-m-201803072039.svg',
+    '../css/sprite/community-m-201803072039.png',
+    'https://ask.qcloudimg.com/raw/ywfjvz7645.jpg',
+    'https://zww-v5.github.io/demos/community-mobile-pwa/css/img/more-icon.png',
+    'https://zww-v5.github.io/demos/community-mobile-pwa/css/img/index/adbg2@2x.jpg',
+    'https://zww-v5.github.io/demos/community-mobile-pwa/css/img/featured-mask.png',
+    'https://zww-v5.github.io/demos/community-mobile-pwa/css/img/index/adbg3@2x.jpg',
+    'https://zww-v5.github.io/demos/community-mobile-pwa/css/img/tags-icon.png',
+    'https://imgcache.qq.com/open_proj/proj_qcloud_v2/gateway/community/css/img/temp/avatar.jpg'
+];
 var dataCacheName = 'Tencent-cloud-data-1.0';
 
 self.addEventListener('install', function(e) {
