3e886ea43a7fc792852bb0e425a4d965911d2c55
zww_v5 <zhuweiwu@foxmail.com>
2018-03-11 13:20:02 +0800

    [+] 增加了pwa的示例

---
+++ community-mobile-pwa/temp-pwa/service-worker.js
@@ -0,0 +1,23 @@
+var cacheName = 'Tencent-cloud-1.0';
+var filesToCache = ['index.html','../css/community-m-dist.css','../css/sprite/community-m-201803072039.svg','../css/sprite/community-m-201803072039.png'];
+var dataCacheName = 'Tencent-cloud-data-1.0';
+
+self.addEventListener('install', function(e) {
+    console.log('[ServiceWorker] Install');
+    e.waitUntil(caches.open(cacheName).then(function(cache) {
+        console.log('[ServiceWorker] Caching app shell');
+        return cache.addAll(filesToCache);
+    }));
+});
+
+self.addEventListener('activate', function(e) {
+    console.log('[ServiceWorker] Activate');
+    e.waitUntil(caches.keys().then(function(keyList) {
+        return Promise.all(keyList.map(function(key) {
+            console.log('[ServiceWorker] Removing old cache', key);
+            if (key !== cacheName && key !== dataCacheName) {
+                return caches.delete(key);
+            }
+        }));
+    }));
+});
