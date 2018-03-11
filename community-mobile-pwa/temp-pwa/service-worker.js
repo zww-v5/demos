24ba55ef0daecc3d43007331830a916dfb771375
zww_v5 <zhuweiwu@foxmail.com>
2018-03-11 15:00:51 +0800

    [*] 优化样式

--- community-mobile-pwa/temp-pwa/service-worker.js
+++ community-mobile-pwa/temp-pwa/service-worker.js
@@ -1,29 +1,43 @@
-// var cacheName = 'Tencent-cloud-1.0';
-// var filesToCache = [];
-// var dataCacheName = 'Tencent-cloud-data-1.0';
+var cacheName = 'Tencent-cloud-1.0';
+var filesToCache = [
+    'index.html',
+    'app.js',
+    //'https://imgcache.qq.com/open_proj/proj_qcloud_v2/gateway/portal/css/global.css',
+    '../css/community-m-dist.css',
+    '../css/sprite/community-m-201803072039.svg',
+    '../css/sprite/community-m-201803072039.png',
+    //'https://ask.qcloudimg.com/raw/ywfjvz7645.jpg',
+    '../css/img/more-icon.png',
+    '../css/img/index/adbg2@2x.jpg',
+    '../css/img/featured-mask.png',
+    '../css/img/index/adbg3@2x.jpg',
+    '../css/img/tags-icon.png',
+    //'https://imgcache.qq.com/open_proj/proj_qcloud_v2/gateway/community/css/img/temp/avatar.jpg'
+];
+var dataCacheName = 'Tencent-cloud-data-1.0';
 
-// self.addEventListener('install', function(e) {
-//     console.log('[ServiceWorker] Install');
-//     e.waitUntil(caches.open(cacheName).then(function(cache) {
-//         console.log('[ServiceWorker] Caching app shell');
-//         return cache.addAll(filesToCache);
-//     }));
-// });
-//
-// self.addEventListener('activate', function(e) {
-//     console.log('[ServiceWorker] Activate');
-//     e.waitUntil(caches.keys().then(function(keyList) {
-//         return Promise.all(keyList.map(function(key) {
-//             console.log('[ServiceWorker] Removing old cache', key);
-//             if (key !== cacheName && key !== dataCacheName) {
-//                 return caches.delete(key);
-//             }
-//         }));
-//     }));
-// });
+self.addEventListener('install', function(e) {
+    console.log('[ServiceWorker] Install');
+    e.waitUntil(caches.open(cacheName).then(function(cache) {
+        console.log('[ServiceWorker] Caching app shell');
+        return cache.addAll(filesToCache);
+    }));
+});
+
+self.addEventListener('activate', function(e) {
+    console.log('[ServiceWorker] Activate');
+    e.waitUntil(caches.keys().then(function(keyList) {
+        return Promise.all(keyList.map(function(key) {
+            console.log('[ServiceWorker] Removing old cache', key);
+            if (key !== cacheName && key !== dataCacheName) {
+                return caches.delete(key);
+            }
+        }));
+    }));
+});
 
 self.addEventListener('fetch', function(e) {
-    // e.respondWith(caches.match(e.request).then(function(response) {
-    //     return response || fetch(e.request);
-    // }));
+    e.respondWith(caches.match(e.request).then(function(response) {
+        return response || fetch(e.request);
+    }));
 });
