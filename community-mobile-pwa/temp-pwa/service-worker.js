76ce2fa0c6ee52f2c514fbbf431b40cb8deb9c0e
zww_v5 <zhuweiwu@foxmail.com>
2018-03-14 23:29:54 +0800

    [*] 优化样式

--- community-mobile-pwa/temp-pwa/service-worker.js
+++ community-mobile-pwa/temp-pwa/service-worker.js
@@ -38,6 +38,7 @@ self.addEventListener('activate', function(e) {
 });
 
 self.addEventListener('fetch', function(e) {
+    console.log('[ServiceWorker] Fetch', e.request.url);
     e.respondWith(caches.match(e.request).then(function(response) {
         return response || fetch(e.request);
     }));
