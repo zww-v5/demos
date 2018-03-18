d6e39158e67627a696694f4fdf77889f61dd15e4
zww_v5 <zhuweiwu@foxmail.com>
2018-03-18 22:41:36 +0800

    [*] 优化内容

--- community-mobile-pwa/temp-pwa/service-worker.js
+++ community-mobile-pwa/temp-pwa/service-worker.js
@@ -1,4 +1,4 @@
-var cacheName = 'Tencent-cloud-1.2';
+var cacheName = 'Tencent-cloud-1.3';
 var filesToCache = [
     '/demos/community-mobile-pwa/temp-pwa/',
     '/demos/community-mobile-pwa/temp-pwa/index.html',
