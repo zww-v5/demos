09de7044e2f916afed096459a4ba9bf793e7e696
zww_v5 <zhuweiwu@foxmail.com>
2018-09-04 22:47:42 +0800

    [*]webpack练习

--- webpack/first-test2/src/index.js
+++ webpack/first-test2/src/index.js
@@ -1,22 +1,17 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
-import Data from './data.xml';
+import PrintMe from './print.js';
+import printMe from './print.js';
 
 function component() {
   var element = document.createElement('div');
+  var btn = document.createElement('button');
 
-  // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
 
-  // 将图像添加到现有的div
-  var myIcon = new Image();
-  myIcon.src = Icon;
+  btn.innerHTML = 'click me and check the console';
+  btn.onclick = printMe;
 
-  element.appendChild(myIcon);
-
-  console.log(Data);
+  element.appendChild(btn);
 
   return element;
 }
