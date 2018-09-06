149869061dcd3ac3ed8ba5e71df9ba81a4801af7
zww_v5 <zhuweiwu@foxmail.com>
2018-09-06 23:38:54 +0800

    [*]webpack练习

--- webpack/first-test2/src/index.js
+++ webpack/first-test2/src/index.js
@@ -1,28 +1,18 @@
-import _ from 'lodash';
-import printMe from './print.js';
+import { cube } from './math.js';
 
-function component() {
-  var element = document.createElement('div');
-  var btn = document.createElement('button');
+if (process.env.NODE_ENV !== 'production') {
+  console.log('Looks like we are in development mode!');
+}
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+function component(){
+  var element = document.createElement('pre');
 
-  btn.innerHTML = 'click me and check the console';
-  btn.onclick = printMe;
-
-  element.appendChild(btn);
+  element.innerHTML = [
+    'Hello webpack',
+    '5 cubed is equal to ' + cube(5)
+  ].join('\n\n');
 
   return element;
 }
 
-let element = component();
-document.body.appendChild(element);
-
-if (module.hot) {
-  module.hot.accept('./print.js', function(){
-    console.log('Accepting the updated printMe module!');
-    document.body.moveClild(element);
-    element = component();
-    document.body.appendChild(element);
-  });
-}
+document.body.appendChild(component());
