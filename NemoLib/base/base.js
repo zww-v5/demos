0d31ccb909f335b6cb9f7616f108affb83b120c9
zww_v5 <zhuweiwu@foxmail.com>
2018-09-28 23:37:51 +0800

    [*]完善基础脚本

--- NemoLib/base/base.js
+++ NemoLib/base/base.js
@@ -339,20 +339,142 @@
     }
 
     /**
-     * testAlert
+     * isFunction
+     * @param {Object} 要判断的对象
+     */
+    funcs.isFunction = function(obj){
+        return typeof obj === "function" && typeof obj.nodeType !== "number";
+    }
+
+    /**
+     * isPlainObject
+     * @param {Object} 要判断的对象
+     */
+    funcs.isPlainObject = function(obj){
+        var proto, Ctor;
+
+		// Detect obvious negatives
+		// Use toString instead of jQuery.type to catch host objects
+		if ( !obj || toString.call( obj ) !== "[object Object]" ) {
+			return false;
+		}
+
+		proto = Object.getPrototypeOf( obj );
+
+		// Objects with no prototype (e.g., `Object.create( null )`) are plain
+		if ( !proto ) {
+			return true;
+		}
+
+		// Objects with prototype are plain iff they were constructed by a global Object function
+		Ctor = hasOwn.call( proto, "constructor" ) && proto.constructor;
+		return typeof Ctor === "function" && fnToString.call( Ctor ) === ObjectFunctionString;
+    }
+
+    /**
+     * extend
+     * @param {Boolean} 是否要深度拷贝，非必填项
+     * @param {Object} 要拷贝的对象，第1个对象是目标对象
+     * ... 同第二个参数，个数无限
+     */
+    funcs.extend = function(){
+        var options, name, src, copy, copyIsArray, clone,
+            target = arguments[ 0 ] || {},
+            i = 1,
+            length = arguments.length,
+            deep = false;
+
+        // Handle a deep copy situation
+        if ( typeof target === "boolean" ) {
+            deep = target;
+
+            // Skip the boolean and the target
+            target = arguments[ i ] || {};
+            i++;
+        }
+
+        // Handle case when target is a string or something (possible in deep copy)
+        if ( typeof target !== "object" && !func.isFunction( target ) ) {
+            target = {};
+        }
+
+        // Extend jQuery itself if only one argument is passed
+        if ( i === length ) {
+            target = this;
+            i--;
+        }
+
+        for ( ; i < length; i++ ) {
+
+            // Only deal with non-null/undefined values
+            if ( ( options = arguments[ i ] ) != null ) {
+
+                // Extend the base object
+                for ( name in options ) {
+                    src = target[ name ];
+                    copy = options[ name ];
+
+                    // Prevent never-ending loop
+                    if ( target === copy ) {
+                        continue;
+                    }
+
+                    // Recurse if we're merging plain objects or arrays
+                    if ( deep && copy && ( funcs.isPlainObject( copy ) ||
+                        ( copyIsArray = Array.isArray( copy ) ) ) ) {
+
+                        if ( copyIsArray ) {
+                            copyIsArray = false;
+                            clone = src && Array.isArray( src ) ? src : [];
+
+                        } else {
+                            clone = src && funcs.isPlainObject( src ) ? src : {};
+                        }
+
+                        // Never move original objects, clone them
+                        target[ name ] = funcs.extend( deep, clone, copy );
+
+                    // Don't bring in undefined values
+                    } else if ( copy !== undefined ) {
+                        target[ name ] = copy;
+                    }
+                }
+            }
+        }
+
+        // Return the modified object
+        return target;
+    }
+
+    /**
+     * parseHTML
      * @param {String} 参数一
      */
-    funcs.testAlert = function(){
-        alert('hello world');
+    funcs.parseHTML = function(htmlString){
+        if ( typeof htmlString !== "string" ) {
+            return [];
+        }
+
+        var context = document.implementation.createHTMLDocument();
+        context.body.innerHTML = htmlString;
+        return tmp.body.children;
+    }
+
+    /**
+     * type //输出对象的 typeof 字体串
+     * @param {Object} 参数一
+     */
+    funcs.type = function(obj){
+        return Object.prototype.toString.call(obj).replace(/^\[object (.+)\]$/, '$1').toLowerCase();
     }
 
     /**
      * testAlert
      * @param {String} 参数一
      */
-    funcs.testAlert = function(){
-        alert('hello world');
-    }
+    // funcs.testAlert = function(){
+    //     alert('hello world');
+    // }
 
     /*
 getJSON
@@ -360,12 +482,6 @@ ajax
     Post
     request
 fadeIn //直接使用 move.js 或 animate.css 代替
-
-
-deep extend
-extend
-parse html
-type
     */
 
     if (window.NEMO) {
